d1b0c445910e9fb0cb4c7f13ce6e6c1ca9742877
igor-bobrov <i.bobrov.nhaa@gmail.com>
2024-08-06 02:15:02 +0700

    6.8.2024

--- init-dwm.sh
+++ init-dwm.sh
@@ -1,7 +1,14 @@
 ﻿#!/bin/bash
+chmod +x ~/sakari-dwm/xinit.sh
+~/sakari-dwm/xinit.sh
 
 git clone https://git.suckless.org/dwm
 echo 'exec dwm' >> ~/.xinitrc
 cd dwm
 sudo make clean install
 cd
+
+# settings
+echo 'numlockx on' >> ~/.xinitrc
+echo 'sudo pacman -Syu' >> ~/.xinit.rc
+
---
+++ xinit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo pacman -Sy dmenu --noconfirm
+
+# Установка Xorg 
+sudo pacman -Sy xorg xorg-xinit xorg-server --noconfirm
+
+
